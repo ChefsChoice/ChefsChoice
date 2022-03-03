abcb4541805571abdfdfd0afea053f078679c26f
Meme Scott <82485727+employeeofchoice@users.noreply.github.com>
2022-03-03 13:29:38 -0500

    Switched to basic barcode scanner

--- myapplication/src/main/java/com/example/myapplication/MainActivityMLK.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivityMLK.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
@@ -8,6 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,85 +17,42 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlin.math.roundToInt
 
 class MainActivityMLK : AppCompatActivity() {
-    // Request code for camera intent
-    // Secret handshake that authenticates for correct result
-    val REQUEST_IMAGE_CAPTURE = 1
 
-    // Create variables for RecyclerView
-    val labelList = mutableListOf<String>()
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var manager: RecyclerView.LayoutManager
-    private lateinit var myAdapter: LabelAdapter
+        private lateinit var mQrResultLauncher : ActivityResultLauncher<Intent>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.tvLabel)
-        manager = LinearLayoutManager(this)
+            // Alternative to "onActivityResult", because that is "deprecated"
+            mQrResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if(it.resultCode == Activity.RESULT_OK) {
+                    val result = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
 
-        // Click listener set on Button to launch camera activity
-        findViewById<FloatingActionButton>(R.id.button).setOnClickListener {
-            dispatchTakePictureIntent(REQUEST_IMAGE_CAPTURE)
-        }
-    }
-
-    private fun dispatchTakePictureIntent(requestCode: Int) {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, requestCode)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Check if requestCode matches REQUEST_IMAGE_CAPTURE value
-        // Check if result was OK
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            // Unpack Intent data to retrieve image as Bitmap
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-
-            // Create InputImage object
-            val image = InputImage.fromBitmap(imageBitmap, 0)
-            // Create instance of ImageLabeler
-            val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
-            // Process InputImage using labeler
-            labeler.process(image)
-                .addOnSuccessListener { labels ->
-                    // Task completed successfully
-                    for (label in labels) {
-                        val text = label.text
-                        val confidence = label.confidence * 100
-                        val index = label.index
-                        labelList.add("$text : ${"%.1f".format(confidence)}%")
-                        Log.d("ImageRecognition", "$text : $confidence")
-                    }
-                    recyclerView.apply {
-                        myAdapter = LabelAdapter(labelList)
-                        layoutManager = manager
-                        adapter = myAdapter
+                    if(result.contents != null) {
+                        // Do something with the contents (this is usually a URL)
+                        println(result.contents)
                     }
                 }
-                .addOnFailureListener { e ->
-                    // Task failed with an exception
-                    // ...
-                }
+            }
 
-           // val options = BarcodeScannerOptions.Builder()
-            //    .setBarcodeFormats(
-            //        Barcode.FORMAT_QR_CODE,
-             //       Barcode.FORMAT_AZTEC)
-            //   .build()
+            // Starts scanner on Create of Overlay (you can also call this function using a button click)
+            startScanner()
+        }
 
-            // Bind Bitmap image to ImageView. Unsure of if this is still necessary
-           // findViewById<ImageView>(R.id.capturedImage).setImageBitmap(imageBitmap)
+
+        // Start the QR Scanner
+        private fun startScanner() {
+            val scanner = IntentIntegrator(this)
+            // QR Code Format
+            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            // Set Text Prompt at Bottom of QR code Scanner Activity
+            scanner.setPrompt("QR Code Scanner Prompt Text")
+            // Start Scanner (don't use initiateScan() unless if you want to use OnActivityResult)
+            mQrResultLauncher.launch(scanner.createScanIntent())
         }
     }
-}
-
