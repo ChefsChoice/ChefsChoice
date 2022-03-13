3d374c38fa7273a51ebdc9360fb46841ad265bca
Tyrees Carney <Tyrees34@gmail.com>
2022-03-13 15:10:19 -0700

    added firebase login

--- myapplication/src/main/java/com/example/myapplication/MainActivity.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,137 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //constants
+    private companion object{
+        private const val RC_SIGN_IN = 100
+        private const val TAG = "GOOGLE_SIGN_IN_TAG"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val secondActbutton = findViewById<Button>(R.id.secondActivityBtn)
-        secondActbutton.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        //configure the google SignIn
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(R.string.default_web_client_id.toString())
+            .requestEmail() // we only need email from google account
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //Google SignIn Button
+        val googleSignInBtn= findViewById<Button>(R.id.googleSignInBtn)
+        googleSignInBtn.setOnClickListener {
+            Log.d(TAG, "onCreate: begin Google SignIn")
+            val intent = this.googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
 
         }
     }
 
-}
+    private fun checkUser() {
+      val firebaseUser = firebaseAuth.currentUser
+      if (firebaseUser != null){
+          startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+          finish()
+      }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            Log.d(TAG, "onActivityResult: Google SignIn intent result")
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+
+            } catch (e: Exception) {
+
+                Log.d(TAG, "onActivityResult: ${e.message}")
+            }
+
+            if (accountTask.isSuccessful) {
+                //Add your next activity in here
+                startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+            } else {
+                //If login fails
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth with google account")
+
+        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: LoggedIn")
+
+                val firebaseUser = firebaseAuth.currentUser
+
+                val uid = firebaseUser!!.uid
+                val email = firebaseUser.email
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Uid: $uid")
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Email: $email")
+
+                if (authResult.additionalUserInfo!!.isNewUser){
+
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Account created... \n$email")
+                    Toast.makeText(this@MainActivity, "Account created... \n$email", Toast.LENGTH_SHORT).show()
+                }
+                else {
+
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Existing user... \n$email")
+                    Toast.makeText(this@MainActivity, "LoggedIn... \n$email", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+                finish()
+
+                }
+            .addOnFailureListener { e ->
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Login Failed due to ${e.message}")
+                Toast.makeText(this@MainActivity, "Login Failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+
+        }
+    }
+
+
+
+
 
--- myapplication/src/main/java/com/example/myapplication/MainActivity2.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,19 +1,38 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+
+private lateinit var firebaseAuth: FirebaseAuth
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.fragmentContainerView)
 
         bottomNavigationView.setupWithNavController(navController)
     }
+
+    private fun checkUser() {
+
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
+        else{
+            val email = firebaseUser.email
+        }
+    }
+
 }
